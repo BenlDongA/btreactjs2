8e2d1d755d5f07620ebfefb6f561a886d0fcd924
BenlDongA <nguyencuongbenl@gmail.com>
2023-09-02 01:12:09 +0700

    bt1reactjs

--- src/components/blogs.js
+++ src/components/blogs.js
@@ -18,7 +18,7 @@
               />
                <Iteam
                 image="https://blog.logrocket.com/wp-content/uploads/2021/04/getting-started-with-create-react-app.png"
-                title="HƯỚNG DẪN HỌC REACT JS"
+                title="HƯỚNG DẪN CẤU TRÚC THƯ MỤC VÀ CÁCH VIẾT COMCOMPONENT CHUẨN TRONG REACT"
                 time="Posted by anh benl | Th2 26 12 2016 | Javascript."
                 stat={
                   <>
@@ -29,7 +29,7 @@
               />
               <Iteam
                 image="https://techvccloud.mediacdn.vn/2020/7/13/137-1594616701190893786687-crop-15946167118531494150206.png"
-                title="HƯỚNG DẪN HỌC REACT JS"
+                title="HƯỚNG DẪN CÀI ĐẶT WEBPACK ĐỂ VIẾT REACTJS/ES6"
                 time="Posted by anh benl | Th2 26 12 2016 | Javascript."
                 stat={
                   <>
@@ -40,7 +40,7 @@
               />
                 <Iteam
                 image="https://hiu.vn/wp-content/uploads/2023/05/next-js-training.png"
-                title="HƯỚNG DẪN HỌC REACT JS"
+                title="HƯỚNG DẪN SỬ DỤNG REDUX HIỆU QUẢ TRONG ỨNG DUNG REACT "
                 time="Posted by anh benl | Th2 26 12 2016 | Javascript."
                 stat={
                   <>
@@ -51,7 +51,7 @@
               />
                 <Iteam
                 image="https://lh3.googleusercontent.com/00hjufFKhWveAIuUfk5CfBL4FI0I4TdFMJ0KunpCPr1SMFDcAYM8q6g3vrW27DNVhCHyiDyXcjifJN2XTGwdNNWs7n0QAR8zePHALaRTQ9d7qBIL7lBGtw5XeEvbvbN_s7r1oMaY"
-                title="HƯỚNG DẪN HỌC REACT JS"
+                title="VÒNG ĐỜI CỦA COMPONENT TRONG REACT JS CS6"
                 time="Posted by anh benl | Th2 26 12 2016 | Javascript."
                 stat={
                   <>
